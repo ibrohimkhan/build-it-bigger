d2369e4365996f08f8c14f2bf6b30d1296c563f6
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2020-03-01 20:13:01 +0500

    commiting missed files

---
+++ backend/src/main/java/com/udacity/backend/MyBean.java
@@ -0,0 +1,13 @@
+package com.udacity.backend;
+
+public class MyBean {
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
